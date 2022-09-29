a1b408640e95c487ade35a7445f201693e0b1ed3
Ekamba <axelgassay@yahoo.dk>
2022-09-29 20:10:48 +0200

    cors issue fixed

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ app.use(
   express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 })
 );
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRouter);
--- models/post.js
+++ models/post.js
@@ -1,10 +1,10 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
   postCreator: String,
   title: String,
   body: String,
-  author: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  author: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   imageFile: String,
   likes: { type: [String], default: [] },
   createdAt: {
