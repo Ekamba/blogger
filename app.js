c40a397d320976081c04441a8185c8c7a845c95b
Ekamba <axelgassay@yahoo.dk>
2022-12-11 18:58:45 +0100

    edited app.js

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const app = express();
 const __dirname = path.dirname("index.html");
 
 const corsOptions = {
-  origin: "https://blogger-client.netlify.app",
+  // origin: "https://blogger-app.onrender.com",
+  origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
